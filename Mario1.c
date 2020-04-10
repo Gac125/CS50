75a3efd03ddb120f348c3083d9f70f6325aab3fb
Gac125 <giulia.castro12@gmail.com>
2020-04-10 14:28:20 -0300

    Week 1 - Mario

--- Mario1.c
+++ Mario1.c
@@ -1,20 +1,31 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void)
-{
-    int n;
-    do{
-        n = get_int("Height:"); //get the height 
+int main(void){
+  int height; //variable of the height if the pyramid
+   do{
+    height = get_int("Height:"); //get the height 
+  } 
+  while (height < 1 || height > 8); // if height is not between 1 and 8 get it again
+    
+  if (height == 1) {
+    printf("#\n");
+  }
+
+  else{
+    int i = height - 1;//number of spaces in the first line 
+    int e = 1; //# in the frist line 
+
+    while (i >= 0){
+      for (int counter = 0; counter < i; counter++){
+        printf(" "); //print the exact number of spaces
+      }
+      for (int counter1 = 0; counter1 < e; counter1++){            
+        printf("#"); //print the exact number of #
+      } 
+      printf("\n");
+      i--;
+      e++;
     }
-    while(n<=1 || n>8); //get the height again if n is not between 1 and 8, inclusive
-
-    int i = n-1;
-    int e = 1;
-
-    while (i>=0){
-      printf(" "*i,"#"*e,"\n");
-      e+=1;
-      i-=1;
-    }
+  }
 }
